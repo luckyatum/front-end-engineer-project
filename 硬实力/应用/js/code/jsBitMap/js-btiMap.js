1f8595f9bee492471bd7e8bc665dcfb23845a3e4
Ou JiaMin <2439768801@qq.com>
2019-11-07 21:34:05 +0800

    bitMap算法

--- 硬实力/应用/js/code/jsBitMap/js-btiMap.js
+++ 硬实力/应用/js/code/jsBitMap/js-btiMap.js
@@ -5,23 +5,38 @@
  * @date 2019-11-07 18:16
  */
 
-const inputData = require('./bitMapTestData')
-
-console.log('inputData', inputData)
-enter(inputData)
-
 // 定义全局数据
 const global = {
-
+    data: {
+        ids: []
+    }
 }
 
 // 入口
 function enter(data) {
+    global.data.ids = data.map(i => i.id);
     // 组装数据
-    assembleData(data)
+    assembleData(data);
 }
 
 // 组装数据
-assembleData(data) {
+function assembleData(data) {
+    const bitsMap = {};
+    if (data.length < 0) {
+        return [];
+    }
 
+    const firstRow = data[0];
+    const firstRowKeys = Object.keys(firstRow);
+    firstRowKeys.forEach(i => {
+        if (i !== 'id' && i !== 'name') {
+            // 默认长度为4的数组
+            bitsMap[i] = new Array(4).fill('00000000');
+        }
+    });
+    console.log('bitsMap', bitsMap);
 }
+
+
+const inputData = require('./bitMapTestData').data;
+enter(inputData)
