82c768701f1147575ba5e52139b1ff4a275774f5
小白 <wb-ojm618300@alibaba-inc.com>
2019-11-07 18:37:40 +0800

    添加bitmap算法

---
+++ 硬实力/应用/js/code/jsBitMap/bitMapTestData.js
@@ -0,0 +1,47 @@
+exports.data = [
+  {
+    id: 1,
+    name: 'user1',
+    sex: 'man',
+    phone: 'apple',
+    age: 18,
+    profession: 'frontEngineer',
+    interest: 'sing'
+  },
+  {
+    id: 5,
+    name: 'user2',
+    sex: 'woman',
+    phone: 'samsung',
+    age: 64,
+    profession: 'frontEngineer',
+    interest: 'swim'
+  },
+  {
+    id: 23,
+    name: 'user3',
+    sex: 'man',
+    phone: 'huawei',
+    age: 18,
+    profession: 'chef',
+    interest: 'skate'
+  },
+  {
+    id: 45,
+    name: 'user4',
+    sex: 'woman',
+    phone: 'huawei',
+    age: 2,
+    profession: 'baby',
+    interest: 'sing'
+  },
+  {
+    id: 1002,
+    name: 'user5',
+    sex: 'man',
+    phone: 'huawei',
+    age: 33,
+    profession: 'frontEngineer',
+    interest: 'sing'
+  }
+]
