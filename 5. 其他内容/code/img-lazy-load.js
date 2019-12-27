074cabc6af78f307d4a94edd0dd89f76a90ac65f
luckyatum <2439768801@qq.com>
2019-12-27 16:14:23 +0800

    修改格式

--- 5. 其他内容/code/img-lazy-load.js
+++ 5. 其他内容/code/img-lazy-load.js
@@ -1,50 +1,50 @@
 // 图片懒加载代码实现
 // html部分代码--<img class="lazyimg" data-src="./images/hd_01-three-hdcg.png" alt="">
 var index = {
-    loadImgs: null, // 需要懒加载的图片数组
-    imgLoadLock: null,
-    init: function () {
-        var _this = this;
-        _this.loadImgs = $('img.lazyimg');
-        $('body,html').scrollTop(0);
-        $(window).on('scroll', function () {
-            // 处理图片懒加载
-            if (_this.loadImgs.length > 0) {
-                if (_this.imgLoadLock != null) {
-                    clearTimeout(_this.imgLoadLock);
-                }
-                _this.imgLoadLock = setTimeout(function () {
-                    _this.imgHandle();
-                }, 100);
-            }
-        });
-    },
-    // 处理图片懒加载
-    imgHandle: function () {
-        var _this = this;
-        $.each(_this.loadImgs, function (i, el) {
-            var $el = $(el),
-                dsrc = $(el).attr('data-src'),
-                isLoadImg = _this.isLoadImg($el),
-                isShow = _this.chekImgShow($el);
-            if (isShow && !isLoadImg) {
-                $el.attr('src', dsrc);
-            }
-        });
-    },
-    // 检查图片是否显示
-    chekImgShow: function ($img) {
-        var scrollTop = $(window).scrollTop();  //即页面向上滚动的距离
-        var windowHeight = $(window).height(); // 浏览器自身的高度
-        var offsetTop = $img.offset().top;  //目标标签img相对于document顶部的位置
-        if (offsetTop < (scrollTop + windowHeight + 150) && offsetTop > scrollTop) { //在2个临界状态之间的就为出现在视野中的
-            return true;
+  loadImgs: null, // 需要懒加载的图片数组
+  imgLoadLock: null,
+  init: function () {
+    var _this = this
+    _this.loadImgs = $('img.lazyimg')
+    $('body,html').scrollTop(0)
+    $(window).on('scroll', function () {
+      // 处理图片懒加载
+      if (_this.loadImgs.length > 0) {
+        if (_this.imgLoadLock != null) {
+          clearTimeout(_this.imgLoadLock)
         }
-        return false;
-    },
-    // 图片是否已加载
-    isLoadImg: function ($img) {
-        return $img.attr('data-src') === $img.attr('src');
+        _this.imgLoadLock = setTimeout(function () {
+          _this.imgHandle()
+        }, 100)
+      }
+    })
+  },
+  // 处理图片懒加载
+  imgHandle: function () {
+    var _this = this
+    $.each(_this.loadImgs, function (i, el) {
+      var $el = $(el)
+                var dsrc = $(el).attr('data-src')
+                var isLoadImg = _this.isLoadImg($el)
+                var isShow = _this.chekImgShow($el)
+      if (isShow && !isLoadImg) {
+        $el.attr('src', dsrc)
+      }
+    })
+  },
+  // 检查图片是否显示
+  chekImgShow: function ($img) {
+    var scrollTop = $(window).scrollTop() // 即页面向上滚动的距离
+    var windowHeight = $(window).height() // 浏览器自身的高度
+    var offsetTop = $img.offset().top // 目标标签img相对于document顶部的位置
+    if (offsetTop < (scrollTop + windowHeight + 150) && offsetTop > scrollTop) { // 在2个临界状态之间的就为出现在视野中的
+      return true
     }
-};
-index.init();
+    return false
+  },
+  // 图片是否已加载
+  isLoadImg: function ($img) {
+    return $img.attr('data-src') === $img.attr('src')
+  }
+}
+index.init()
--- 5. 其他内容/code/util.js
+++ 5. 其他内容/code/util.js
@@ -1,25 +1,25 @@
 // js实现二分查找算法
-function findIndex(arr, num) {
-	if (arr.length == 0) {
-		return -1;
-	}
-	var lIndex = 0;
-	var rIndex = arr.length - 1;
-	var mIndex;
+function findIndex (arr, num) {
+  if (arr.length == 0) {
+    return -1
+  }
+  var lIndex = 0
+  var rIndex = arr.length - 1
+  var mIndex
 
-	while(true) {
-		mIndex = parseInt((rIndex + lIndex) / 2);
+  while (true) {
+    mIndex = parseInt((rIndex + lIndex) / 2)
 
-		if (arr[mIndex] > num) {
-			rIndex = mIndex - 1;
-		} else if (arr[mIndex] < num) {
-			lIndex = mIndex + 1;
-		} else {
-			return mIndex;
-		}
+    if (arr[mIndex] > num) {
+      rIndex = mIndex - 1
+    } else if (arr[mIndex] < num) {
+      lIndex = mIndex + 1
+    } else {
+      return mIndex
+    }
 
-		if (rIndex < lIndex) {
-			return -1;
-		}
-	}
+    if (rIndex < lIndex) {
+      return -1
+    }
+  }
 }
