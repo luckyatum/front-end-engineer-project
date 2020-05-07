c7b3a6e0caeb7f86980f1f3d1d36509bf8c794ab
Ou JiaMin <2439768801@qq.com>
2020-05-07 20:55:26 +0800

    增加monthly博客

--- 5. 其他内容/code/imooc前端实战项目/api/src/api/PublicController.js
+++ 5. 其他内容/code/imooc前端实战项目/api/src/api/PublicController.js
@@ -3,8 +3,16 @@ import svgCaptcha from "svg-captcha";
 class PublicController {
   constructor() {}
   async getCaptcha(ctx) {
-    const newCaptcha = svgCaptcha.create({});
+    const newCaptcha = svgCaptcha.create({
+      size: 4,
+      ignoreChars: 'Oo0li1',
+      color: true,
+      noise: Math.floor(Math.random() * 5),
+      width: 150,
+      height: 50
+    });
     ctx.body = {
+      code: 200,
       msg: newCaptcha.data
     };
   }
